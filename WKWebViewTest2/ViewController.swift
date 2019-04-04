399edc02fd91b5a976a040d4011b6b34ad970b60
litulei <litulei9@163.com>
2019-04-04 15:34:08 +0800

    Initial Commit

---
+++ WKWebViewTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WKWebViewTest2
+//
+//  Created by bitu on 2019/4/4.
+//  Copyright © 2019 bitu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
